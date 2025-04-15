993d394927c847b8ed74e40550a311fc792d042b
TSMT3366 <letaison0303@gmail.com>
2025-04-15 10:48:07 +0700

    Add files via upload

---
+++ Bài_6-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+void hoan_doi (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void in_MT (int a[])
+{
+    for (int i=0; i<9; i++)
+    {
+        printf ("%5d", a[i]);
+    }
+    printf ("\n");
+}
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    in_MT(A);
+    int a, b;
+    printf ("Chon 2 vi tri muon hoan doi: ");
+    scanf ("%d %d", &a, &b);
+    hoan_doi(&A[a], &A[b]);
+    in_MT(A);
+    return 0;
+}
