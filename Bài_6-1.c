d53f669362a57e27464d40dd5bff7cb88088c734
TSMT3366 <letaison0303@gmail.com>
2025-04-15 11:39:31 +0700

    Update Bài_6-1.c

--- Bài_6-1.c
+++ Bài_6-1.c
@@ -7,7 +7,7 @@ void hoan_doi (int *a, int *b)
     *b = temp;
 }
 
-void in_MT (int a[])
+void in_MT (int *a)
 {
     for (int i=0; i<9; i++)
     {
@@ -18,11 +18,11 @@ void in_MT (int a[])
 int main()
 {
     int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    in_MT(A);
+    in_MT(&A);
     int a, b;
     printf ("Chon 2 vi tri muon hoan doi: ");
     scanf ("%d %d", &a, &b);
     hoan_doi(&A[a], &A[b]);
-    in_MT(A);
+    in_MT(&A);
     return 0;
 }
